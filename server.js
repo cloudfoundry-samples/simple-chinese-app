2599b0bf3277963c49b1d082b6458b64a12b0a24
Shash Reddy <sreddy@pivotal.io>
2016-03-18 11:54:24 -0700

    Sample Node JS app to print chinese characters
    Signed-off-by: Atul Kshirsagar <atul.kshirsagar@gmail.com>

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+
+HOST = null;
+
+var host = "0.0.0.0";
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html;charset=GBK'});
+    res.write('<h1>Hello from a node app! ');
+    res.write('</h1>');
+    res.write('<h2>');
+    res.write('未经授权的访问 ');
+    res.write('via: ' + host + ':' + port);
+    res.end('</h2>');
+}).listen(port, null);
+
+console.log('Server running at http://' + host + ':' + port + '/');
